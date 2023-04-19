ed3b1c831f095fe882e4642c0c2138d630f56beb
danjovich <78160019+danjovich@users.noreply.github.com>
2023-04-19 00:56:21 -0300

    fix: is_neg should verify if is <0, not <= 0

--- src/implementations/lau.c
+++ src/implementations/lau.c
@@ -2,7 +2,7 @@
 
 bool is_zero(int16_t v) { return v == 0; }
 
-bool is_neg(int16_t v) { return v <= 0; }
+bool is_neg(int16_t v) { return v < 0; }
 
 int16_t add(int16_t a, int16_t b) { return a + b; }
 
