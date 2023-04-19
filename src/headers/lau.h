5d5c93a3177e17577798b6cb757ecc4af97a2cb8
danjovich <78160019+danjovich@users.noreply.github.com>
2023-04-18 21:25:27 -0300

    feat: MVN LAU

---
+++ src/headers/lau.h
@@ -0,0 +1,59 @@
+#ifndef VETORES_H_
+#define VETORES_H_
+#include "types.h"
+
+#include <stdint.h>
+
+/**
+ * @brief Checks if a value is 0.
+ *
+ * @param v the value to check
+ * @return true if v == 0, else false
+ */
+bool is_zero(int16_t v);
+
+/**
+ * @brief Checks if a value is lower than 0.
+ *
+ * @param v the value to check
+ * @return true if v < 0, else false
+ */
+bool is_neg(int16_t v);
+
+/**
+ * @brief Sums two values.
+ *
+ * @param a
+ * @param b
+ * @return a + b
+ */
+int16_t add(int16_t a, int16_t b);
+
+/**
+ * @brief Subtracts two values.
+ *
+ * @param a
+ * @param b
+ * @return a - b
+ */
+int16_t sub(int16_t a, int16_t b);
+
+/**
+ * @brief Multiplies two values.
+ *
+ * @param a
+ * @param b
+ * @return a * b
+ */
+int16_t mul(int16_t a, int16_t b);
+
+/**
+ * @brief Divides two values.
+ *
+ * @param a
+ * @param b
+ * @return a / b
+ */
+int16_t div(int16_t a, int16_t b);
+
+#endif // VETORES_H_
