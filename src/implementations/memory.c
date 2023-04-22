91bae3efe3f7b9364cbb622d27667c93c9737bda
danjovich <78160019+danjovich@users.noreply.github.com>
2023-04-22 00:12:13 -0300

    add: register based memory getter and setter

--- src/implementations/memory.c
+++ src/implementations/memory.c
@@ -1,4 +1,17 @@
-#include <stdint.h>
+#include "../implementations/registers.c"
 
+#include <stdint.h>
+#include <stdio.h>
 // 4.095 kB memory
-int8_t memory[0xFFF] = {0x00};
+#define MEMORY_SIZE 0xFFF
+
+uint8_t memory[MEMORY_SIZE] = {0x00};
+
+void get_from_memory() {
+  regs.MDR = (memory[regs.MAR] * 0x100) + memory[regs.MAR + 0x1];
+}
+
+void set_memory(int16_t value) {
+  memory[regs.MAR] = value / 0x100;
+  memory[regs.MAR + 0x1] = value;
+}
