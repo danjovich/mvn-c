ed7e3c3fa6a60b236a4e9b6a0b31cf4ee4b141c6
danjovich <78160019+danjovich@users.noreply.github.com>
2023-04-22 00:11:03 -0300

    update: change registers type to unsigned

--- src/include/registers.h
+++ src/include/registers.h
@@ -8,27 +8,27 @@
  */
 typedef struct registers {
   // Memory Address Register, it saves the address to be got from the memory
-  int16_t MAR;
+  uint16_t MAR;
 
   // Memory Data Register, it saves the value returned from the memory
-  int16_t MDR;
+  uint16_t MDR;
 
   // Instruction Register, it saves the instruction got at the beginning of each
   // cycle
-  int16_t IR;
+  uint16_t IR;
 
   // OPeration, it saves the operation to be executed at the cycle
-  int16_t OP;
+  uint16_t OP;
 
   // Operand Instruction, it saves the operand of the instruction
-  int16_t OI;
+  uint16_t OI;
 
   // ACcumulator, register that is used to save values gotten from various
   // places
-  int16_t AC;
+  uint16_t AC;
 
   // Instruction Counter, it is used to save the address of the next instruction
-  int16_t IC;
+  uint16_t IC;
 } Registers;
 
 #endif // REGISTERS_H_
