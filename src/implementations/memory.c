a1547165200bf992192f6da12c69234f5c528cdf
danjovich <78160019+danjovich@users.noreply.github.com>
2023-04-26 19:31:27 -0300

    fix: set_memory function argument was signed, generating unexpected behavior sometimes

--- src/implementations/memory.c
+++ src/implementations/memory.c
@@ -11,7 +11,7 @@ void get_from_memory() {
   regs.MDR = (memory[regs.MAR] * 0x100) + memory[regs.MAR + 0x1];
 }
 
-void set_memory(int16_t value) {
+void set_memory(uint16_t value) {
   memory[regs.MAR] = value / 0x100;
   memory[regs.MAR + 0x1] = value;
 }
